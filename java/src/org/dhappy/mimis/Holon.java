4270fe1b713b1d492b1b78a531b4a226c8f356cd
Will Holcomb <will@dhappy.org>
2013-07-05 11:18:38 -0400

    clearing out old code

--- java/src/org/dhappy/mimis/Holon.java
+++
@@ -1,9 +0,0 @@
-package org.dhappy.mimis;
-
-import java.util.Map;
-import org.neo4j.graphdb.Node;
-
-public interface Holon {
-    public Node getNode();
-    public void impress( Map<String, Object> state );
-}
--- java/src/org/dhappy/mimis/Listing.java
+++
@@ -1,8 +0,0 @@
-package org.dhappy.mimis;
-
-import org.neo4j.graphdb.Traverser;
-
-public interface Listing extends Traverser, Holon {
-    public Holon top();
-    public boolean hasNext();
-}
