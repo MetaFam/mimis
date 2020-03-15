4b8fd50f7ef37206924788ff14e6b5b96c0173fc
Will Holcomb <will@dhappy.org>
2020-03-15 19:15:00 -0400

    addin' readium lite reader 🥴

--- src/Readium/index.jsx
+++ src/Readium/index.jsx
@@ -1,15 +1,5 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Readium } from '@evidentpoint/readium-js'
+import { Button } from 'antd'
 
-console.log(Readium)
-
-export default (props) => {
-  useEffect(() => {
-    const readiumOptions = { jsLibRoot: '/' }
-    const readerOptions = { el: '#reader' }
-    const readium = new Readium(readiumOptions, readerOptions)
-    readium.openPackageDocument(props.url)
-  }, [props.url])
-
-  return <div id='reader'></div>
-}
+export default (props) => <iframe src={`/readium/?epub=${props.url}`}/>
