db0f91baaa35d914d8121250fb7a1717cfd71bd9
Will Holcomb <dys@dhappy.org>
2021-04-20 09:56:46 -0400

    Updates

--- static/js/3.be726677.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpmimis=this.webpackJsonpmimis||[]).push([[3],{79:function(n,e,t){"use strict";t.r(e);var r=t(84),i=t(6),c=t(2);e.default=function(n){var e=n.value,t=n.colorFor,a=void 0===t?function(){return"transparent"}:t;return Object(c.jsx)(r.c,{p:12,bg:"orange",children:e.map((function(n,e){return Object(c.jsx)(r.a,{bg:a({index:e}),children:Object(c.jsx)(i.default,{value:n})},e)}))})}}}]);
-//# sourceMappingURL=3.be726677.chunk.js.map
