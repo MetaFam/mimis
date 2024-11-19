09ed9b248668319d5a2507b69fd80d224eee5a0c
δυς <d+gh@dhappy.org>
2024-11-19 01:47:24 -0500

    not comprehendin’ `actor`'s `Task`s 🍯

--- bin/stream-unixfs.mjs
+++ bin/stream-unixfs.mjs
@@ -1,90 +1,135 @@
 #!/usr/bin/env node
 
-import { create as ipfsClient } from 'kubo-rpc-client'
-import { prepare, encode, code as pbCode } from '@ipld/dag-pb'
-import { UnixFS } from 'ipfs-unixfs'
-import { sha256 } from 'multiformats/hashes/sha2'
-import { CID } from 'multiformats/cid'
 import { CarWriter } from '@ipld/car'
-import { Readable } from 'node:stream'
+import * as UnixFS from '@ipld/unixfs'
+import * as Rabin from '@ipld/unixfs/file/chunker/rabin'
+import * as Trickle from '@ipld/unixfs/file/layout/trickle'
+import * as RawLeaf from 'multiformats/codecs/raw'
+import { sha256 } from 'multiformats/hashes/sha2'
 import fs from 'node:fs/promises'
 
-const max = 25
-const ipfs = ipfsClient('http://localhost:5001')
-const genCAR = true
-const doInsert = false
-const outFile = await fs.open('output.car', 'w')
-
-try {
-  const links = [], blocks = []
-
-  for(const i of Array.from({ length: max }).map((_, i) => (i + 1))) {
-    const content = `Hello, ${i} World!`
-    const file = new UnixFS({
-      type: 'raw',
-      data: new TextEncoder().encode(content),
-      hashType: BigInt(sha256.code),
-      // mtime: new Date(),
-      mode: 0o644,
-    })
-    const fileNode = prepare({ Data: file.marshal(), Links: [] })
-    const fileBytes = encode(fileNode)
-    let cid
-    if(doInsert) {
-      cid = await ipfs.block.put(fileBytes, { format: 'dag-pb' })
+const iterate = async function* (stream) {
+  const reader = stream.getReader()
+  for(;;) {
+    const next = await reader.read()
+    if(next.done) {
+      return
+    } else {
+      yield next.value
     }
-    if(!cid) {
-      const hash = await sha256.digest(fileBytes)
-      cid = CID.createV1(pbCode, hash)
-    }
-    blocks.push({ bytes: fileBytes, cid })
-    links.push({ Hash: cid, Name: `hello #${i}.txt`, Tsize: Number(file.fileSize()) })
   }
+}
 
-  const dir = new UnixFS({
-    type: 'directory',
-    // mtime: new Date(),
-    mode: 0o755,  
+const pipe = async (source, writer) => {
+  for await (const item of source) {
+    writer.write(item)
+  }
+  return await writer.close()
+}
+
+const encodeCAR = (blocks, rootCID) => {
+  const { writer, out } = CarWriter.create([rootCID])
+  pipe(iterate(blocks), {
+    write: ({ cid, bytes }) => writer.put({ cid, bytes }),
+    close: () => writer.close(),
   })
-  const dirNode = (
-    prepare({
-      Data: dir.marshal(),
-      Links: links,
-    })
-  )
-  const dirBytes = encode(dirNode)
-  let cid
-  if(doInsert) {
-    cid = await ipfs.block.put(dirBytes, { format: 'dag-pb' })
-  }
-  if(!cid) {
-    const hash = await sha256.digest(dirBytes)
-    cid = CID.createV1(pbCode, hash)
-  }
-  blocks.push({ bytes: dirBytes, cid })
+  return out
+}
 
-  if(genCAR) {
-    const { writer, out } = await CarWriter.create([cid])
-    const writeable = outFile.createWriteStream()
-    
-    const readable = Readable.from(out)
-    const writePromise = new Promise((resolve, reject) => {
-      writeable.on('finish', resolve)
-      writeable.on('error', reject)
-    })
-    readable.pipe(writeable)
+const streamToBlob = async (
+  stream, type = 'application/octet-stream',
+) => {
+  const chunks = []
+  const reader = stream.getReader()
 
-    for(const block of blocks) {
-      await writer.put(block)
+  try {
+    for(;;) {
+      const { done, value } = await reader.read()
+      if(done) break
+      chunks.push(value)
     }
-
-    await writer.close()
-    await writePromise
-    await outFile.close()
+  } finally {
+    reader.releaseLock()
   }
 
-  console.info(`Final CID: ${cid}`)
-} catch(error) {
+  const length = chunks.reduce(
+    (total, chunk) => total + chunk.length, 0,
+  )
+  const concatenated = new Uint8Array(length)
+
+  let offset = 0
+  for(const chunk of chunks) {
+    concatenated.set(chunk, offset)
+    offset += chunk.length
+  }
+
+  return new Blob([concatenated], { type })
+}
+
+const saveBlobToFile = async (blob, filename) => {
+  const arrayBuffer = await blob.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+  await fs.writeFile(filename, buffer)
+}
+
+const main = async () => {
+  console.info('Starting CAR Creation…')
+
+  const {
+    readable: transformReadable,
+    writable: transformWritable,
+  } = new TransformStream()
+  const unixFSWriter = UnixFS.createWriter({
+    writable: transformWritable,
+    settings: {
+      fileChunker: await Rabin.create(),
+      fileLayout: Trickle.configure({ maxDirectLeaves: 100 }),
+      fileChunkEncoder: RawLeaf,
+      smallFileEncoder: RawLeaf,
+      fileEncoder: UnixFS,
+      hasher: sha256,
+    },
+  })
+
+  console.info('Created UnixFS Writer…')
+
+  const dir = UnixFS.createDirectoryWriter(unixFSWriter)
+
+  const range = [...Array(10).keys()].map((i) => (i + 1))
+
+  console.info({ 'Iterating Over': range })
+
+  for(const i of range) {
+    console.info(`Writing hello #${i}…`)
+    const content = `Hello, ${i} World!`
+    const file = UnixFS.createFileWriter(unixFSWriter)
+    file.write(new TextEncoder().encode(content))
+    console.info(`Wrote Content: ${content}`)
+    const { cid } = await file.close()
+    console.debug({ cid })
+    const name = `hello #${i}.txt`
+    dir.set(name, cid)
+    console.info(`Wrote ${name} with CID ${cid}.`)
+  }
+  const root = await dir.close()
+  await unixFSWriter.close()
+
+  const car = encodeCAR(transformReadable, root)
+  const blob = await streamToBlob(
+    car, 'application/vnd.ipld.car',
+  )
+  const filename = 'output.car'
+  await saveBlobToFile(blob, filename)
+
+  console.info(`CAR file written to ${filename}.`)
+  console.info({ 'Final CID': root })
+
+  return root.cid
+}
+
+main()
+.then((cid) => console.debug({ Got: cid }))
+.catch((error) => {
   console.error(`Error: ${error.message}`)
   console.error(error.stack)
-}
+})
