4fa388f1ef0fa3274dadfc1c90514c84b2a0d526
δυς <dys@dhappy.org>
2024-12-25 20:25:09 -0500

    workin’ on documentation 🍅

--- pkgs/mimis-svelte/src/routes/about/+page.svelte
+++ pkgs/mimis-svelte/src/routes/about/+page.svelte
@@ -6,26 +6,45 @@
 	/>
 </svelte:head>
 
-<section class="text-column">
+<section>
 	<h1>About Mïmis</h1>
 
-	<p>Mïmis is a collaborative filesystem. It functions by publishing <a href="https://ipld.io/docs/codecs/known/dag-cbor/">CBOR-DAG</a>s describing context trees to <a href="https://ipfs.io">IPFS</a> where they are picked up & cached in a <a href="https://neo4j.org">Neo4j</a> instance.</p>
+	<p>Mïmis is a collaborative filesystem. It functions by publishing <a href="https://ipld.io/docs/codecs/known/dag-pb">ProtoBuf-DAG</a> UnixFS file systems describing context trees to <a href="https://ipfs.io">IPFS</a> where they are picked up & cached in a <a href="https://neo4j.org">Neo4j</a> instance using content-addressing to deduplicate across versions in a manner reminiscent of <code>git</code>.</p>
 
 	<img src="architecture.svg" alt="Mïmis Architecture"/>
 
-	<p>Each user has their own tree and it is possible to browse and subscribe to sections of other users' trees. These subscriptions can be "mounted" into a composite tree beginning with the user's tree and merging mounted nodes into it.</p>
+	<p>Each user has their own tree, and it is possible to browse and subscribe to sections of other users' trees. These subscriptions can be "union mounted" into a composite tree beginning with the user's tree, and merging mounted nodes into it.</p>
 
-	<p>When a context path is ambiguous, the system can be requested to return an ordered list of possible candidate resources.</p>
+	<p>When a context path is ambiguous, the system can be requested to return the highest-priority answer or an ordered list of possible candidate resources.</p>
 
-	<p>One function of that these ordered lists will enable is a content recommender. Users can create ordered lists ast different context points, and they're combined & collated to generate a likely list of the consensus opinion.</p>
+	<p>One function of that these ordered lists will enable is a content recommender. Users can create ordered lists at different context points, and they're combined & collated to generate a likely list of the consensus opinion.</p>
 
-	<p>Structurally, the context tree differs from a traditional filesystem in that files don't have names, per se. Their "name" is simply the file type. <em>(Oftentimes, this is duplicated, like <code>svg.svg</code> because a variety of softwares rely on there being an extension.)</em> Because there is no context information in the filename, it is possible to create multiple routes to a file without the fear that the filename relies on context in the leading path.</p>
+	<p>Structurally, the context tree differs from a traditional filesystem in that files don't have names, per se. Their "name" is simply the file type. <em>(Oftentimes, this is duplicated, like <code inert>svg.svg</code> because a variety of softwares rely on there being an extension.)</em> Because there is no context information in the filename, it is possible to create multiple routes to a file without the fear that the filename relies on context in the leading path.</p>
 
-	<p>The goal is that the tree should contain a variety of paths describing a resource.</p>
+	<p>The goal is that the tree should contain a variety of paths describing a resource rather than a single fixed point.</p>
+
+	<p></p>
 </section>
 
 <style>
+	section {
+		margin-block: 2rlh;
+	}
+
 	img {
 		margin-block: 2rem;
+		max-height: 95svh;
+		margin-inline: auto;
+	}
+
+	p {
+		initial-letter: 2 2;
+		text-indent: 1em;
+		margin-block-start: 0.25lh;
+		margin-trim: block;
+		text-wrap: pretty;
+		max-width: 66ch;
+		margin-inline: auto;
+		text-align: justify;
 	}
 </style>
--- pkgs/mimis-svelte/src/routes/settings/+page.svelte
+++ pkgs/mimis-svelte/src/routes/settings/+page.svelte
@@ -1,4 +1,4 @@
-<script type="ts">
+<script lang="ts">
   import { settings } from '$lib/settings.svelte';
 </script>
 
